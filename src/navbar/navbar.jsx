d24519f8ef942562b43b4e46c80075cbc46308dd
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-05 16:23:43 -0500

    added tailwind

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import '../App.css'
 
 function Navbar() {
   return (
     <div>
       <navbar>
 
-        <p>xxxxxxxxxxxxxxxxxxx</p>
+        <h1 className="text-3xl font-bold underline font-sans hover:font-serif">
+          Hello world!
+        </h1>
+
+        <p className='font-sans md:font-mono'>PIES</p>
 
       </navbar>
 
